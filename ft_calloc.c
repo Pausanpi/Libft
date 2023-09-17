54844d97b12360b658d9e8f596cdf1d15af67bb0
Paula Sánchez <123211878+Pausanpi@users.noreply.github.com>
2023-09-17 21:22:13 +0200

    Add files via upload

---
+++ ft_calloc.c
@@ -0,0 +1,13 @@
+#include "libft.h"
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*ptr;
+	
+	ptr = (void*)malloc(nmemb * size);
+	if (!ptr)
+		return (NULL);
+	ft_bzero(ptr, nmemb);
+	return (ptr);
+}
+
---
+++ ft_strdup.c
@@ -0,0 +1,15 @@
+#include "libft.h"
+
+char	*ft_strdup(const char *s)
+{
+	char	*tmp;
+	size_t	len;
+	
+	len = ft_strlen(s) + 1;
+	tmp = malloc(sizeof(char) * len);
+	if (!tmp)
+		return (NULL);
+	rtn = ft_memcpy(tmp, s, len);
+	return (tmp);
+}
+
