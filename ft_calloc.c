de1094c209f3b88322db91128c8151ac4e2b2fd5
Paula Sánchez <123211878+Pausanpi@users.noreply.github.com>
2023-09-18 15:34:30 +0200

    Update ft_calloc.c

--- ft_calloc.c
+++ ft_calloc.c
@@ -3,8 +3,8 @@
 void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*ptr;
-	
-	ptr = (void*)malloc(nmemb * size);
+
+	ptr = (void *)malloc(nmemb * size);
 	if (!ptr)
 		return (NULL);
 	ft_bzero(ptr, nmemb);
